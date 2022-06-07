23a5fa7d8ffcf83f986947aa38bf5753529fed9c
Nikolay Rys <nikolay@rys.me>
2022-06-07 07:10:30 +0300

    Enable delaying in tests, since we control it manually

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -2,6 +2,5 @@ Delayed::Worker.sleep_delay = 5
 Delayed::Worker.max_attempts = 3
 Delayed::Worker.max_run_time = 5.minutes
 Delayed::Worker.default_queue_name = 'default'
-Delayed::Worker.delay_jobs = !Rails.env.test?
 Delayed::Worker.raise_signal_exceptions = :term
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
