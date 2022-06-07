9240b2fa98a2d3c78749d5345a964a8647d421cf
Nikolay Rys <nikolay@rys.me>
2022-06-07 03:57:55 +0300

    Add abstract controller to handle authorizations

---
+++ app/controllers/secured_controller.rb
@@ -0,0 +1,17 @@
+class SecuredController < ApplicationController
+  before_filter :authenticate!
+
+  # We're not really relying on current_user, but it's a good example of what is usually done
+  attr_reader :current_user
+
+  private
+  def authenticate!
+    @current_user = User.find(auth_token[:user_id])
+  rescue TokenError, ActiveRecord::RecordNotFound
+    render json: { errors: ['Not Authenticated'] }, status: :unauthorized
+  end
+
+  def auth_token
+    TokenService.decode(request.headers['Authorization']&.split(' ')&.last)
+  end
+end
